e4619abdad984c3b502533d4c907f9f9b0a3c70f
sigall <speledleviatan@gmail.com>
2020-11-06 11:05:28 +0200

    KO ranksum full code

--- Analysis/KO_ranksum.py
+++ Analysis/KO_ranksum.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 from scipy.stats import mannwhitneyu
 from statsmodels.stats.multitest import fdrcorrection
 
@@ -33,3 +34,49 @@ def test_enrichment(temp_K0_res, rel_kos_df, rel_p):
         ko_enrichment_res['fdr'] = fdrcorrection(ko_enrichment_res['natural_pval'])[1]
     ko_enrichment_res = ko_enrichment_res.sort_values('fdr')
     return ko_enrichment_res
+
+
+if __name__ == "__main__":
+    base_path = "//wiz-d2/atlas_project"
+    corr_path = os.path.join(base_path, 'ko_correlations')
+
+    all_modules_list = pd.read_csv(os.path.join(base_path, 'kegg_db/list_of_modules.csv'))[['index', 'name']]
+
+    # Creating a binary df from kegg genes and pathways/modules
+    gene_to_path = pd.read_csv(os.path.join(base_path, 'gene_to_path.csv'))
+    pathways = pd.get_dummies(gene_to_path.set_index('kegg_gene')['path']).max(level=0).reset_index()
+
+    gene_to_module = pd.read_csv(os.path.join(base_path, 'gene_to_module.csv'))
+    modules = pd.get_dummies(gene_to_module.set_index('kegg_gene')['module']).max(level=0).reset_index()
+
+    cols = {'KEGG_KO': 'kegg_gene', 'Spearman correlation P value': 'spear_pval',
+            'Spearman correlation coefficient': 'spear'}
+    # RankSum Mann-Whitney Test for pathway/module enrichment for different phenotypes
+    for pheno in ['hba1c', 'bmi', 'age', 'bt__hdl_cholesterol', 'bt__fasting_triglycerides', 'bt__fasting_glucose']:
+        kegg = pd.read_csv(os.path.join(corr_path, 'final_atlas_5_%s.csv' % pheno), skipinitialspace=True)
+        kegg.rename(columns=cols, inplace=True)
+
+        kegg['pval_log_sign'] = -(np.log(kegg.spear_pval) * np.sign(kegg.spear))
+        kegg.sort_values('pval_log_sign', ascending=False, inplace=True)
+
+        kegg['rank'] = range(kegg.shape[0])
+        kegg = kegg[['kegg_gene', 'rank', 'pval_log_sign']].set_index('kegg_gene')
+
+        rel_kos_pathways_df = pathways.copy()
+        rel_kos_modules_df = modules.copy()
+
+        # partial pathways that appear in converted data
+        rel_kos_pathways_df = rel_kos_pathways_df[rel_kos_pathways_df.kegg_gene.isin(kegg.kegg_gene)]
+        rel_kos_pathways_df.set_index('kegg_gene', inplace=True)
+
+        pathway_ko_enrichment_res = test_enrichment(kegg, rel_kos_pathways_df, pathways.columns[1:])
+        print("For %s got %d pathways" % (pheno, len(pathway_ko_enrichment_res[pathway_ko_enrichment_res.fdr <= 0.05])))
+        pathway_ko_enrichment_res.to_csv(os.path.join(base_path, 'insights_max', '%s_paths.csv' % pheno))
+
+        rel_kos_modules_df = rel_kos_modules_df[rel_kos_modules_df.kegg_gene.isin(kegg.kegg_gene)]
+        rel_kos_modules_df.set_index('kegg_gene', inplace=True)
+
+        module_ko_enrichment_res = test_enrichment(kegg, rel_kos_modules_df, modules.columns[1:])
+        print("For %s got %d modules" % (pheno, len(module_ko_enrichment_res[module_ko_enrichment_res.fdr <= 0.05])))
+        module_ko_enrichment_res = pd.merge(module_ko_enrichment_res.reset_index(), all_modules_list, on='index')
+        module_ko_enrichment_res.to_csv(os.path.join(base_path, 'insights_max', '%s_module.csv' % pheno))
