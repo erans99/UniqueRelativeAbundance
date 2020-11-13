4bc35233a95486a19ea09fd10c15d32e5b201447
daphna <dafna.aloni@gmail.com>
2020-11-13 10:16:29 -0800

    Simpler code

--- BuildDB/buildRepresentatives.py
+++ BuildDB/buildRepresentatives.py
@@ -8,75 +8,82 @@ from multiprocessing import Pool
 
 
 def renameInputFastaFiles(inp):
-    SelectedSGBs, output_cores_dir, genomes_dir=inp
-    all_contig_lens=[]
-    all_contig_ids=[]
-    for sgb in SelectedSGBs:
-        matchingFasta=glob.glob(os.path.join(genomes_dir,'SGB_%s_*.fa'%sgb))
-        coreContigs=[]
-        if len(matchingFasta)!=1:
-            print("no core found for %s under %s"%(sgb,genomes_dir))
+    SelectedSGBs, output_cores_dir, genomes_dir = inp
+    print(SelectedSGBs)
+    all_contig_lens = []
+    all_contig_ids = []
+    for idx in SelectedSGBs.index:
+        sgb = SelectedSGBs.loc[idx, 'SGB']
+        genome = SelectedSGBs.loc[idx, 'nameOfFile']
+        print(genome)
+        print(genomes_dir)
+        matchingFasta = glob.glob(os.path.join(genomes_dir, '%s.fa' % genome))
+        coreContigs = []
+        if len(matchingFasta) != 1:
+            print("no core found for %s under %s" % (sgb, genomes_dir))
             continue
-        matchingFasta=matchingFasta[0]
-        coreFasta = os.path.join(output_cores_dir, os.path.basename(matchingFasta))
+        matchingFasta = matchingFasta[0]
+        coreFasta = os.path.join(output_cores_dir, 'SGB_%s_%s.fa' % (sgb, genome))
         if not os.path.exists(coreFasta):
             count = 0
             for record in SeqIO.parse(matchingFasta, "fasta"):
-                genome = os.path.basename(matchingFasta).split('SGB_%s_'%sgb)[1][:-3]
                 record.id = 'SGB_%s_c_%s_%s' % (sgb, count, genome)
                 all_contig_ids.append(record.id)
-                count+=1
+                count += 1
                 coreContigs.append(record)
                 all_contig_lens.append(len(record))
-            SeqIO.write(coreContigs,coreFasta ,'fasta')
-    pd.Series(index=all_contig_ids,data=all_contig_lens).to_csv(\
-        os.path.join(output_cores_dir, 'all_contigs.txt'),sep='\t',header=False)
-    print ("Done writing core chunk fasta")
+            SeqIO.write(coreContigs, coreFasta, 'fasta')
+    pd.Series(index=all_contig_ids, data=all_contig_lens).to_csv( \
+        os.path.join(output_cores_dir, 'all_contigs.txt'), sep='\t', header=False)
+    print("Done writing core chunk fasta")
 
-def buildByCore(SelectedSGBs,output_fasta,genomes_dir):
-    if os.path.exists( output_fasta ):
-        os.system( "rm -f %s" % output_fasta )
+
+def buildByCore(SelectedSGBs, output_fasta, genomes_dir):
+    if os.path.exists(output_fasta):
+        os.system("rm -f %s" % output_fasta)
     first = True
-    assert len(SelectedSGBs)==len(set(SelectedSGBs))
+    assert len(SelectedSGBs) == len(set(SelectedSGBs))
     for sgb in SelectedSGBs:
-        matchingFasta=glob.glob(os.path.join(genomes_dir,'SGB_%s_*.fa'%sgb))
-        if len(matchingFasta)!=1:
-            print("no core found for %s under %s"%(sgb,genomes_dir))
+        matchingFasta = glob.glob(os.path.join(genomes_dir, 'SGB_%s_*.fa' % sgb))
+        if len(matchingFasta) != 1:
+            print("no core found for %s under %s" % (sgb, genomes_dir))
             continue
-        matchingFasta=matchingFasta[0]
+        matchingFasta = matchingFasta[0]
         if first:
-            os.system('cat %s > %s'%(matchingFasta,output_fasta))
+            os.system('cat %s > %s' % (matchingFasta, output_fasta))
             first = False
         else:
             os.system('cat %s >> %s' % (matchingFasta, output_fasta))
-    print ("Done writing one big fasta for bowtie index")
+    print("Done writing one big fasta for bowtie index")
 
-def run(SelectedSGBs,configFile):
+
+def run(SelectedSGBs, configFile):
     config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
     config.read(configFile)
     build_representatives = config['build_representatives']
-    print ("Initializing")
-    
+    print("Initializing")
+
     if not os.path.exists(build_representatives['output_cores_dir']):
         os.makedirs(build_representatives['output_cores_dir'])
     num_threads = eval(build_representatives['num_threads'])
-    basedir = build_representatives['qp_base_dir']
+    basedir = build_representatives['base_path']
     os.chdir(basedir)
-    print ("Starting")
-    print (time.ctime())
+    print("Starting")
+    print(time.ctime())
     p = Pool(num_threads)
-    waiton=[]
+    waiton = []
     chunk_size = eval(build_representatives['chunksize'])
-    for chunk in range(0,len(SelectedSGBs), chunk_size):
-        waiton.append((SelectedSGBs[chunk:chunk+chunk_size],
-                                     build_representatives['output_cores_dir'],
-                                     build_representatives['genomes_dir']))
-    p.map(renameInputFastaFiles,waiton)
-   
-    buildByCore(SelectedSGBs,build_representatives['output_fasta'],
-                    build_representatives['output_cores_dir'])
+    for chunk in range(0, len(SelectedSGBs), chunk_size):
+        waiton.append((SelectedSGBs[chunk:chunk + chunk_size],
+                       build_representatives['output_cores_dir'],
+                       build_representatives['genomes_dir']))
+    p.map(renameInputFastaFiles, waiton)
 
-    print (time.ctime())
+    buildByCore(SelectedSGBs['SGB'], build_representatives['output_fasta'],
+                build_representatives['output_cores_dir'])
 
-if __name__=='__main__':
+    print(time.ctime())
+
+
+if __name__ == '__main__':
     run()
