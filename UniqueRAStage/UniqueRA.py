1733382fb37abccb6e6331126026b0e46b3c2534
daphna <dafna.aloni@gmail.com>
2020-11-16 16:28:36 -0800

    Simpler code

--- UniqueRAStage/UniqueRA.py
+++ UniqueRAStage/UniqueRA.py
@@ -1,10 +1,12 @@
+import configparser
 import logging
 import os
 import sys
 import glob
 import time
 
-import map_reads, analyse_maps, score_maps
+from BuildDB.createDistantConfigFile import createDistantConfigFile
+from UniqueRAStage import map_reads, analyse_maps, score_maps
 
 DB_PATH = '/net/mraid08/export/genie/Microbiome/Data/Databases/URA_IndicesAndScores'
 
@@ -21,7 +23,8 @@ def _tryrm(f):
 
 def main(finput, out_dir, read_len, num_mapped_to_subsample=None, min_mapped_to_retain=1000000,
          databases_path=DB_PATH, run_type='LargeOrNewGenusSGBs', only_perfect=False, exp_th=20,
-         min_partial=0.5, min_sc_best=-40, keep_intermediate_files=False, min_abund=10**-4):
+         min_partial=0.5, min_sc_best=-40, keep_intermediate_files=False, min_abund=10**-4,
+         bowtie_mapper='bowtie2',samtools_exe='samtools',unique_n=100):
     """
     Computes relative abundance by unique reads
     :param finput: SE fastq file
@@ -37,6 +40,8 @@ def main(finput, out_dir, read_len, num_mapped_to_subsample=None, min_mapped_to_
     :param min_sc_best: Minimum score of map top use (if not only perfect maps), float
     :param keep_intermediate_files: Whether to keep, or remove, intermidiate files used for estimation process, bool
     :param min_abund: threshold to cut minimum abundance estimation, float
+    :param bowtie_mapper bowtie2 executable path
+    :param samtools_exe samtools executable path
     :return: map_perc - the percenta of reads of the samples mapped to index
              num_reads - if negative, minus the number of reads that mapped, which did not pass the minimal needed
                          if positive, the number of reads used for computing relative abundances
@@ -53,7 +58,7 @@ def main(finput, out_dir, read_len, num_mapped_to_subsample=None, min_mapped_to_
     if only_perfect:
         min_sc_best = 0
     out_sam_file, map_perc, num_mapped = \
-        map_reads.run_sample(sample, finput, out_dir, index_name, min_sc_best)
+        map_reads.run_sample(sample, finput, out_dir, index_name, min_sc_best,bowtie_mapper=bowtie_mapper,samtools_exe=samtools_exe)
     _log.info('Aligned {}% for {}'.format(map_perc, sample))
 
     if num_mapped < min_mapped_to_retain:
@@ -80,7 +85,7 @@ def main(finput, out_dir, read_len, num_mapped_to_subsample=None, min_mapped_to_
     scores_dict_files = os.path.join(index_path, 'scores_%d' % read_len, 'dict_scores_%s')
     bins_dict_files = os.path.join(index_path, 'scores_%d' % read_len, 'dict_bins_%s')
     score_maps.run_sample(sample, out_score_file, out_dir, scores_dict_files, exp_th, min_partial, bins_dict_files,
-                          min_abund)
+                          min_abund,unique_n)
 
     if not keep_intermediate_files:
         _log.info('Removing intermediate analysis file')
@@ -91,39 +96,34 @@ def main(finput, out_dir, read_len, num_mapped_to_subsample=None, min_mapped_to_
     _log.info('Done with sample {}'.format(sample))
     return map_perc, num_used
 
-
-if __name__ == '__main__':
-    if len(sys.argv) < 5:
-        print("python3 UniqueRAStage.py <in_dir> <in_file_ext> <read_length> <out_dir> <max_reads> <min_reads>")
-        print("<in_dir> input directory, for fastq files")
-        print("<in_file_ext> fastq file extention (usually 'fq' or 'fastq')")
-        print("<read_length> length of reads in fastq (works with 75, 100 or 150)")
-        print("<out_dir> output directory, for temporary files and final output")
-        print("<max_reads> maximal number of mapped reads to use when computing abundances, if 0 use all, default 5e6")
-        print("<min_reads> minimal number of mapped reads to use when computing abundances, ")
-        print("            will not compute if there are not enough reads, default 1e6")
-        sys.exit(0)
-    else:
-        in_path = sys.argv[1]
-        samples = glob.glob(os.path.join(in_path, "*.%s" % sys.argv[2]))
-        read_len = int(sys.argv[3])
-        out_path = sys.argv[4]
-        if len(sys.argv) > 5:
-            num_mapped_to_subsample = int(eval(sys.argv[5]))
-            if num_mapped_to_subsample == 0:
-                num_mapped_to_subsample = None
-                print("Will not subsample")
-        else:
-            num_mapped_to_subsample = 5 * 10**6
-        if len(sys.argv) > 6:
-            min_mapped_to_retain = int(eval(sys.argv[6]))
-        else:
-            min_mapped_to_retain = 10**6
+def get_parameters_for_main(configFile):
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    config.read(configFile)
+    databases_path = config['user_parameters']['databases_path']
+    bowtie_mapper = config['user_parameters']['bowtie_mapper']
+    samtools_exe = config['user_parameters']['samtools_exe']
+    exp_th=int(config['run_pipeline']['exp_th'])
+    run_type = config['run_pipeline']['run_type']
+    keep_intermediate_files=eval(config['run_pipeline']['keep_intermediate_files'])
+    unique_n=int(config['run_pipeline']['unique_n'])
+    read_len = int(config['user_parameters']['read_len'])
+    in_path=config['run_pipeline']['in_path']
+    in_file_ext=config['run_pipeline']['in_file_ext']
+    samples = glob.glob(os.path.join(in_path, "*.%s" % in_file_ext))
+    out_path = config['user_parameters']['out_path']
+    num_mapped_to_subsample = eval(config['run_pipeline']['num_mapped_to_subsample'])
+    min_mapped_to_retain=int(config['run_pipeline']['min_mapped_to_retain'])
 
     for samp in samples:
         _log.info("Started %s at" % samp, time.ctime())
-        map_perc, num_used = main(samp, out_path, read_len, num_mapped_to_subsample, min_mapped_to_retain)
+        map_perc, num_used = main(samp, out_path, read_len, num_mapped_to_subsample, min_mapped_to_retain,databases_path=databases_path, run_type=run_type,
+                                  bowtie_mapper=bowtie_mapper,samtools_exe=samtools_exe,exp_th=exp_th,keep_intermediate_files=keep_intermediate_files,
+                                  unique_n=unique_n)
         if num_used > 0:
             _log.info("%s had %g percent mapping. %d reads where used to asses URA" % (samp, map_perc, num_used))
         else:
             _log.info("%s had %g percent mapping. %d reads where too few asses URA" % (samp, map_perc, -num_used))
+
+if __name__=="__main__":
+    configFile = createDistantConfigFile(forceRewrite=False)
+    get_parameters_for_main(configFile)
