3ed4b7b38356f3f1de042486b87e461808e9fe66
sigall <speledleviatan@gmail.com>
2020-11-18 12:19:31 +0200

    added tools tp config

--- UniqueRAStage/config.py
+++ UniqueRAStage/config.py
@@ -2,3 +2,6 @@ DB_PATH = './'  # put path to DBs here
 DB = 'LargeOrNewGenusSGBs'  # specific DB for this run
 
 NUM_UNIQ_IN_PART = 100  # this is set in building the DB
+
+BOWTIE_MAPPPER = './Bin/bowtie2-2.3.4/bowtie2'  # path to bowtie mapper
+SAM_TOOLS = './Bin/SAMTools/samtools-1.9/bin/samtools'  # path for samtools
